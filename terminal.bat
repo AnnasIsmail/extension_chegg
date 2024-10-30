816bfb71f672f56808595db674daea88e62291d2
Annas Laptop <annasismailmuhammad@gmail.com>
2024-10-31 02:06:44 +0700

    push

--- terminal.bat
+++ terminal.bat
@@ -1,1 +1,5 @@
-for /f "skip=1 tokens=3" %%s in ('query user %USERNAME%') do (%windir%\System32\tscon.exe %%s /dest:console)
+@echo off
+for /f "skip=1 tokens=3" %%s in ('query user %USERNAME%') do (
+    %windir%\System32\tscon.exe %%s /dest:console
+)
+nircmd win activate ititle "Google Chrome"
